7c0c10465c04aadbecdb6f83172e34943fcdf84b
tjonjumy <tjonjumy@gmail.com>
2025-07-24 21:13:57 +0700

    push source

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <h2 class="mt-3 fw-bold">Tra cứu 3321 xã/phường mới của Việt Nam</h2>
+  <div class="mt-3">
+    <label class="label-txt">Tỉnh/Thành</label>
+    <InputText
+      v-model="province"
+      name="province"
+      type="text"
+      placeholder="Tìm kiếm theo tỉnh/thành"
+      class="inp-search"
+    />
+  </div>
+  <div class="mt-3">
+    <label class="label-txt">Xã/Phường</label>
+    <InputText
+      v-model="ward"
+      name="ward"
+      type="text"
+      placeholder="Tìm kiếm theo xã/phường"
+      class="inp-search"
+    />
+  </div>
+  <div class="btn-search mt-3">
+    <Button label="Tìm kiếm" icon="pi pi-search" @click="onSearch" />
+  </div>
+  <DataTable
+    :value="datas"
+    sortField="id"
+    :sortOrder="1"
+    scrollable
+    scrollHeight="400px"
+    paginator
+    :rows="5"
+    :rowsPerPageOptions="[5, 10, 20, 50]"
+    showGridlines
+    tableStyle="min-width: 50rem"
+    tableClass="mt-3"
+  >
+    <Column field="province" header="Tỉnh/Thành"></Column>
+    <Column field="name" header="Xã/Phường"></Column>
+    <Column header="Gộp từ các phường">
+      <template #body="slotProps">
+        {{ slotProps.data?.mergedFrom?.join(", ") }}
+      </template>
+    </Column>
+    <template #empty> Không có dữ liệu </template>
+  </DataTable>
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
+import responseApi from "./data/data.js";
+
+const province = ref("hà nội");
+const ward = ref("");
+const datas = ref([]);
+
+onMounted(() => {
+  datas.value = flattenedData(responseApi.data);
+});
+
+const onSearch = () => {
+  if (province.value) {
+    const provinceQuery = removeVietnameseTones(province.value);
+    datas.value = flattenedData(responseApi.data).filter((element) => {
+      const itemName = removeVietnameseTones(element.province);
+      return itemName.includes(provinceQuery);
+    });
+  } else {
+    datas.value = flattenedData(responseApi.data);
+  }
+
+  if (ward.value) {
+    const wardQuery = removeVietnameseTones(ward.value);
+    datas.value = flattenedData(responseApi.data).filter((element) => {
+      const itemName = removeVietnameseTones(element.name);
+      return itemName.includes(wardQuery);
+    });
+  }
+};
+
+const removeVietnameseTones = (str) => {
+  return str
+    .toLowerCase()
+    .normalize("NFD") // Chuyển thành chuỗi Unicode tổ hợp
+    .replace(/[\u0300-\u036f]/g, "") // Loại bỏ các dấu
+    .replace(/đ/g, "d") // Chuyển đ thành d
+    .replace(/Đ/g, "D");
+};
+
+// Flatten wards
+const flattenedData = (data) => {
+  const demo = data.flatMap((item) =>
+    item.wards.map((ward) => ({
+      id: item.id,
+      province: item.province,
+      name: ward.name,
+      mergedFrom: ward.mergedFrom,
+    }))
+  );
+  console.log(demo);
+  
+  return demo;
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+import App from './App.vue'
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+ });
+app.mount('#app')
